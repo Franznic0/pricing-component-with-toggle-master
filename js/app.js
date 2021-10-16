4ab289f4d6ed5e997a7c96b5aec09404dd173201
Franznic0 <64605498+Franznic0@users.noreply.github.com>
2021-10-16 21:23:22 +0100

    commit to 'main'

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const switchButton = document.getElementById("switch");
+const monthPrice = document.querySelectorAll("month-price");
+const yearPrice = document.querySelectorAll("year-price");
+
+console.log(switchButton);
+
+monthPrice.array.forEach(element => {
+});
